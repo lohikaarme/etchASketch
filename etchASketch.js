c747ae6f928ca754938cba95e5ee7835c6c30463
lohikaarme <stewart.watt@gmail.com>
2022-04-24 00:36:10 -0700

    createGrid function basics setup

---
+++ etchASketch.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('#container');
+
+//container.textContent = 'test';
+
+let createGrid = (cols, rows) => {
+//    container.style.setProperty('grid-template-columns', `repeat(${cols}, 1fr)`);
+container.style.setProperty('--grid-cols', cols);
+container.style.setProperty('--grid-rows', rows);
+for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell')
+        cell.textContent = 'cell';
+        container.appendChild(cell);
+    }
+}
+
+createGrid(10,5)
