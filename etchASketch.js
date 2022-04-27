9e062bfe31895be86d964e8b45529748c1453caf
lohikaarme <stewart.watt@gmail.com>
2022-04-26 22:15:01 -0700

    adjusted default state, added color select logic

--- etchASketch.js
+++ etchASketch.js
@@ -1,36 +1,35 @@
 const container = document.querySelector('#grid');
-const button = document.getElementById('btn');
-//const color = document.getElementById('colorPick');
+const button = document.querySelector('#btn');
+const color = document.querySelector('.color');
 
 let draw = false;
+window.addEventListener('mousedown', () => draw = true);
+window.addEventListener('mouseup', () => draw = false);
 
 let createGrid = (area) => {
     clearChildren(grid);
-    container.style.setProperty('--grid-cols', area);
-    container.style.setProperty('--grid-rows', area);
+    container.style.setProperty('--grid-area', area);
     for (let i = 0; i < (area ** 2); i++) {
         let cell = document.createElement('div');
-        cell.classList.add('cell')
-        //cell.textContent = 'cell';
+        cell.classList.add('cell');
         container.appendChild(cell);
         
+        cell.addEventListener('mousedown', () => {
+            cell.style.backgroundColor = color.value;
+        });
         cell.addEventListener('mouseover', () => {
-            if (!draw) return;
-            cell.style.backgroundColor = 'purple';//color;
+            if (!draw) return; 
+            cell.style.backgroundColor = color.value;
         });
     }
 }
 
-window.addEventListener('mousedown', () => draw = true)
-window.addEventListener('mouseup', () => draw = false)
-
-
 let gridParameters = (area) => {
     area = parseInt(prompt('Grid Size (Max 100)', 16));
-    if (area > 100 || area < 0) {
-        null;
+    if (area <= 100 && area > 0) {
+        createGrid(area);
     } else {
-        createGrid(area)
+        createGrid(16)
     } }
 
 let clearChildren = (parent) => {
@@ -40,4 +39,5 @@ let clearChildren = (parent) => {
 }
 
 button.addEventListener('click', () => gridParameters());
+createGrid(16);
     
